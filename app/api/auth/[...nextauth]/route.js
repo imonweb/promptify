a30241cd4fdbc0e3c4e41f91cc6670fb89888010
Imon <contact@imonweb.co.uk>
2023-06-23 09:05:35 +0100

    next auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,17 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
+// console.log('clientId', process.env.GOOGLE_CLIENT_ID);
+
 const authHandler = NextAuth({
   providers: [GoogleProvider({
-    clientId: "",
-    clientSecret: "",
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   }),
   ],
-  
+
   callbacks: {
-    asynce signIn({profile}){
+    async signIn({profile}){
       try {
 
       } catch (err){
